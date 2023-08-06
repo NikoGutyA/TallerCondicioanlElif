0bbda633ec98ea8e806b9e6698f6b1fcb1c11aa4
Nikol Andrea Gutierrez Agudelo <nikolgutierrezagudelo@gmail.com>
2023-08-05 19:20:59 -0500

    Se termina de desarrollar el punto tres de los condicionales

--- tallerCondicional3.py
+++ tallerCondicional3.py
@@ -1,30 +1,30 @@
 """
-Comparar 4 números solicitados al usuario, escribir condiciones que permita
+Punto 3 Comparar 4 números solicitados al usuario, escribir condiciones que permita
 mostrar que un número fue escrito 2 veces, que todos son iguales o por el contrario
 todos son diferentes.
 """
 
-numero1 =int (input ("escribe un número entero "))
-numero2 =int (input ("escribe un número entero "))
-numero3 =int (input ("escribe un número entero "))
-numero4 =int (input ("escribe un número entero "))
+num1 =int (input ("Escribe un número entero "))
+num2 =int (input ("Escribe un número entero "))
+num3 =int (input ("Escribe un número entero "))
+num4 =int (input ("Escribe un número entero "))
 
-if numero1 == numero2  and numero2== numero3 and numero3 == numero4:
+if num1 == num2  and num2== num3 and num3 == num4:
     print(f"todos los numeros son igules")
-elif numero1==numero2 and numero3==numero4:
-    print(f"el primer y segundo numero estan repetidos y son {numero1} y el tercer y cuarto tambien estan repetidos y el numero es {numero3} ")
-elif numero1==numero2 :
-    print(f"el primer y segundo numero estan repetidos y son {numero1}  ")
-elif numero3==numero4:
-     print(f"el tercer y cuarto numero estan repetidos y son {numero3}  ")
-elif numero1==numero4:
- print(f"el primer y cuarto numero estan repetidos y son {numero1} ")
-elif numero1==numero3:
- print(f"el primer y el tercero numero estan repetidos y son {numero1} ")
+elif num1==num2 and num3==num4:
+    print(f"el primer y segundo numero estan repetidos y son {num1} y el tercer y cuarto tambien estan repetidos y el numero es {numero3} ")
+elif num1==num2 :
+    print(f"el primer y segundo numero estan repetidos y son {num1}  ")
+elif num3==num4:
+     print(f"el tercer y cuarto numero estan repetidos y son {num3}  ")
+elif num1==num4:
+ print(f"el primer y cuarto numero estan repetidos y son {num1} ")
+elif num1==num3:
+ print(f"el primer y el tercero numero estan repetidos y son {num1} ")
 
-elif numero2==numero3:
-     print(f"el segundo y tercero numero estan repetidos y son {numero2} ")
-elif numero2==numero4:
-     print(f"el segundo y el cuarto numero estan repetidos y son {numero2} ")
+elif num2==num3:
+     print(f"el segundo y tercero numero estan repetidos y son {num2} ")
+elif num2==num4:
+     print(f"el segundo y el cuarto numero estan repetidos y son {num2} ")
 else:
-    print("todos los nuemeros son diferentes y son {numero1}, {numero2}, {numero3}, {numero4} ")
+    print("todos los nuemeros son diferentes y son {num1}, {num2}, {num3}, {num4} ")
